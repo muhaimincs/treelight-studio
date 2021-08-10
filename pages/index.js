5894f1924f55667f761f45ed087726f6a9f2bf70
Muhaimin CS <muhaimincs@gmail.com>
2021-08-10 19:17:28 +0800

    adding logo

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,12 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Treelight Studio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header className="flex justify-center items-center">
 
+      </header>
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
